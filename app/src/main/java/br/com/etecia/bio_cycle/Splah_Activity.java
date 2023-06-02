9e4608a4d6afb1213f91e6104cdec2e2d515f585
GabriiellAugustto <gabriel.gauitarra.05@gmail.com>
2023-06-01 22:02:32 -0300

    git

---
+++ app/src/main/java/br/com/etecia/bio_cycle/Splah_Activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.bio_cycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Splah_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splah_layout);
+    }
+}
